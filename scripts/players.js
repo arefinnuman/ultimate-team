fdab2bf9553e117432dcb6fa9cd5b63d1057bafc
arefin <arefinnuman2000@gmail.com>
2022-08-22 01:18:27 +0600

    First Calculation Done

--- scripts/players.js
+++ scripts/players.js
@@ -1,23 +1,34 @@
 const playerList = [];
 
 function display(players){
-
-
     const tableBody = document.getElementById("ultimate-team");
     tableBody.innerHTML = "";
 
     for(let i = 0; i < players.length; i++){
         const name = playerList[i].playerName;
-    
         const tr = document.createElement("tr");
-
         tr.innerHTML = `
         <th>${i+1}</th>
         <td>${name}</td>
         `;
         tableBody.appendChild(tr);
 
+        let playerNumber = playerList.length;
+        // console.log(playerNumber);
+        
     }
+}
+function calculate(players){
+    for(let i = 0; i < players.length; i++){
+        let playerNumber = playerList.length; 
+        document.getElementById('btn-calculate').addEventListener('click', function(){
+            const perPlayer = getValueById("per-player-field");
+            const totalExpenses = perPlayer * playerNumber;
+            setValueById('total-expenses-field', totalExpenses);
+            return totalExpenses;
+            
+        });
+    }   
 }
 
 function addPlayer(element){
@@ -30,6 +41,7 @@ function addPlayer(element){
     playerList.push(playerObj);
 
     display(playerList)
+    calculate(playerList)
 
 }
 
---
+++ scripts/utilities.js
@@ -0,0 +1,11 @@
+function getValueById(elementId){
+    const fieldValue = document.getElementById(elementId);
+    const stringValue = fieldValue.value;
+    const value = parseFloat(stringValue);
+    return value;
+}
+
+function setValueById(elementId, value){
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = value;
+}
